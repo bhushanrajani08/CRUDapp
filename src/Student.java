73fb5beaeb80634c066508998525a7cefd4fba2a
bhushanrajani08 <42447458+bhushanrajani08@users.noreply.github.com>
2023-03-15 19:47:04 +0530

    Add files via upload

---
+++ src/Student.java
@@ -0,0 +1,25 @@
+public class Student {
+    int rollNo;
+    String name, email;
+
+    //getter
+    int getRollNo(){
+        return rollNo;
+    }
+    String getName(){
+        return name;
+    }
+    String getEmail(){
+        return email;
+    }
+    //setter
+    void setRollNo(int rollNo){
+        this.rollNo=rollNo;
+    }
+    void setName(String name){
+        this.name=name;
+    }
+    void setEmail(String email){
+        this.email=email;
+    }
+}
